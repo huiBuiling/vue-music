4897617fc4ed75653f34c1168511eef8066486bd
liuhuihui <244701374@qq.com>
2019-05-23 11:07:21 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "assets/stylus/variable.styl"
+  #app
+    color: $color-theme
+</style>
